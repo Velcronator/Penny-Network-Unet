808752d16da0d59b6e9e7b1570fd8883a651691b
Velcronator <valacos7@gmail.com>
2020-03-17 00:36:06 +1100

    Dealing with Network latency Section 3:

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,22 +1,28 @@
-﻿//adapted from example script available at
+//adapted from example script available at
 //https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
 using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 10.0F;
-    public float rotationSpeed = 100.0F;
+    Rigidbody rb;
+	public float speed = 25.0f;
+    public float rotationSpeed = 50.0f;
 
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();    
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 	
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+        Quaternion turn = Quaternion.Euler(0f, rotation, translation);
+        rb.MovePosition(rb.position + this.transform.forward * translation);
+        rb.MoveRotation(rb.rotation * turn);
 	}
 }
